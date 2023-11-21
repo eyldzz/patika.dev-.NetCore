97556afc4aa76faa8b5d97ddb428ac7575991b8c
Emre Yıldız <yildiz.emree5@gmail.com>
2023-11-21 16:53:36 +0300

    if-elseif-ternary project

---
+++ if-elseif-ternary/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace if_elseif_ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Good morning!");
+            else if (time <= 18)
+                Console.WriteLine("Have a good day!");
+            else
+                Console.WriteLine("Good night!");
+
+            string str = time >= 6 && time < 11 ? "Good morning!" : time <= 18 ? "Have a good day!" : "Good night!";
+            Console.WriteLine(str);
+            
+        }
+    }
+}
