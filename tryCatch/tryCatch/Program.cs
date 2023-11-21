315bcdb6c4111c858f35ccf7fbfed9847d820d0b
Emre Yıldız <yildiz.emree5@gmail.com>
2023-11-21 14:50:55 +0300

    tryCatch project

---
+++ tryCatch/tryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace tryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine(c);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("proccess done");
+            }
+
+
+            try
+            {
+                int a = int.Parse(null);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("cannot be null \n "+ e);
+
+            }
+        }
+    }
+}
