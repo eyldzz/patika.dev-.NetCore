ac1ff0071a21b879c0feff3d0e16d20f3a650b56
Emre Yıldız <yildiz.emree5@gmail.com>
2023-11-24 02:00:17 +0300

    methods(definition) project

---
+++ methods/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace methods
+{
+
+    class Methods
+    {
+        public void print(string _value)
+        {
+            Console.WriteLine(_value);
+        }
+        public int increaseandsum(ref int _a, ref int _b)
+        {
+            _a += 1;
+            _b += 1;
+            return (_a+ _b);
+        }
+    }
+    class Program
+    {
+
+        static int Sum(int _a, int _b)
+        {
+            return _a + _b;
+        }
+          
+        public static void Main(string[] args)
+        {
+            int a = 2, b = 3;
+            Console.WriteLine(a + b);
+            int total= Sum(a, b);
+            Console.WriteLine(total);
+
+            Methods obj = new Methods();
+            obj.print(total.ToString());
+
+            int total2 = obj.increaseandsum(ref a, ref b);
+            obj.print(total2.ToString());
+            obj.print((a + b).ToString());
+
+
+
+        }
+    }
+}
