81affccf674722722da4f1545d1c3dd6124ea4bc
Emre Yıldız <yildiz.emree5@gmail.com>
2023-11-23 12:13:33 +0300

    while-foreach-loops project

---
+++ while-foreach-loops/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace while_foreach_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            Console.Write("enter a integer: ");
+            int number = int.Parse(Console.ReadLine());
+            int total = 0;int counter = 1;
+            while(counter<=number)
+            {
+                total += counter;
+                counter++;
+            }
+            Console.WriteLine(total / number);
+            
+            //foreach
+
+            int[] numbers= {1,2,3,4,5,6,7};
+            foreach(var num  in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
