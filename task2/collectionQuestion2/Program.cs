f52a482b3581529801faa244a8905f72cc5af5c9
Emre Yıldız <yildiz.emree5@gmail.com>
2023-11-29 17:11:44 +0300

    q2 and q3 project

---
+++ task2/collectionQuestion2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+
+namespace collectionQuestion2
+{
+    internal class Program
+    {
+        // Write a program that finds the highest 3 and the lowest 3 of 20 numbers entered from the keyboard
+        // averages the two groups within itself, and prints these averages and average sums to the console. (Write using the Array class.)
+
+        public static void print(int[] param)
+        {
+            foreach(int i in param)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int num = 0;
+            int[] arr = new int[20];
+            for(int i = 0;i < 20; i++)
+            {
+                Console.Write($"Enter {i+1}. integer value: ");
+                if (int.TryParse(Console.ReadLine(), out num))
+                    arr[i] = num;
+                else
+                {
+                    Console.WriteLine("Value must be integer ");
+                    i--;
+                }   
+            }
+            Array.Sort(arr);
+            int[] lows = new int[3];
+            for (int i =0 ; i < 3 ; i++)
+                lows[i] = arr[i];
+            
+            int[] highs = new int[3];
+            int cntr = 0;
+            for(int i = arr.Length-1; i>arr.Length-4;i--)
+            {
+                highs[cntr] = arr[i];
+                cntr++;
+            }
+            Console.Write("Lowest 3 integers: ");
+            print(lows); 
+            Console.Write("\nHighest 3 integers: ");
+            print(highs);
+
+            Console.WriteLine("\n\nLowest 3 integers average: " + lows.Average());
+            Console.WriteLine("Highest 3 integers average: " + highs.Average());
+            int averagessum = (int)(highs.Average() + lows.Average());
+            Console.WriteLine("Lowest and highest averages total: " + averagessum);
+
+
+
+
+        }
+    }
+}
---
+++ task2/collectionQuestion3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace collectionQuestion3
+{
+    //Write a program that stores the vowels in the sentence entered from the keyboard in an array and sorts the elements of the array.
+
+    internal class Program
+    {
+        public static void Vowels(string param)
+        {
+            ArrayList arr = new ArrayList();
+            string vowelwords = "aeıioöuüAEIİOÖUÜ";
+            for (int i = 0;i<param.Length;i++)
+            {
+                if (vowelwords.Contains(param[i]))
+                    arr.Add(param[i]);
+            }
+
+            arr.Sort();
+            Console.Write("sorted vowel words: ");
+            foreach (var i in arr)
+                Console.Write(i + " ");
+        }
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            Vowels(sentence);
+        }
+    }
+}
